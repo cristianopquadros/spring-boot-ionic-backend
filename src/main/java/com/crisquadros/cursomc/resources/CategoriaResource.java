28dbbeb7dc38094b181f8b7c7eb16c5e5eddb864
cristianopquadros <cristianopquadros@gmail.com>
2020-06-30 10:36:55 -0300

    inserindo nova Categoria com post

--- src/main/java/com/crisquadros/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/crisquadros/cursomc/resources/CategoriaResource.java
@@ -1,13 +1,18 @@
 package com.crisquadros.cursomc.resources;
 
+import java.net.URI;
 import java.util.Optional;
+
+import javax.servlet.Servlet;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.crisquadros.cursomc.resources.domain.Categoria;
 import com.crisquadros.cursomc.service.CategoriaService;
@@ -24,10 +29,16 @@ public class CategoriaResource {
 		Optional<Categoria> obj = service.buscar(id);
 		
 		return ResponseEntity.ok().body(obj);
+	
+		}
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj){
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
-				
-		
-		
+		return ResponseEntity.created(uri).build();
 	}
 	
-}
+	}
+
