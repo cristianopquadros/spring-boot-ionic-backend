03168aeae42a18eeaff10874969a2f3f16c4998f
cristianopquadros <cristianopquadros@gmail.com>
2020-09-24 19:14:31 -0300

    Atualizando categoria

--- src/main/java/com/crisquadros/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/crisquadros/cursomc/resources/CategoriaResource.java
@@ -3,8 +3,6 @@ package com.crisquadros.cursomc.resources;
 import java.net.URI;
 import java.util.Optional;
 
-import javax.servlet.Servlet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,7 +24,7 @@ public class CategoriaResource {
 	
 	@RequestMapping(value="/{id}",method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Optional<Categoria> obj = service.buscar(id);
+		Optional<Categoria> obj = service.find(id);
 		
 		return ResponseEntity.ok().body(obj);
 	
@@ -38,6 +36,13 @@ public class CategoriaResource {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		
 		return ResponseEntity.created(uri).build();
+	}
+	
+	
+	public ResponseEntity<Void> update(@RequestBody Categoria obj, @PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
 	}
 	
 	}
