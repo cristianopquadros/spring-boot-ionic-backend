308d6d27d1475464c0a573b28d71e9f8c40f7267
cristianopquadros <cristianopquadros@gmail.com>
2020-10-07 17:48:17 -0300

    implementando tostring de pedido

--- src/main/java/com/crisquadros/cursomc/resources/domain/ItemPedido.java
+++ src/main/java/com/crisquadros/cursomc/resources/domain/ItemPedido.java
@@ -1,6 +1,8 @@
 package com.crisquadros.cursomc.resources.domain;
 
 import java.io.Serializable;
+import java.text.NumberFormat;
+import java.util.Locale;
 
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
@@ -112,6 +114,21 @@ public class ItemPedido implements Serializable{
 		} else if (!id.equals(other.id))
 			return false;
 		return true;
+	}
+
+	@Override
+	public String toString() {
+		NumberFormat nf = NumberFormat.getCurrencyInstance(new Locale("pt", "BR"));
+		StringBuilder builder = new StringBuilder();
+		builder.append(getProduto().getNome());
+	    builder.append(", Qtd: ");
+		builder.append(getQuantidade());
+		builder.append(", preco Unitario: ");
+		builder.append(nf.format(getPreco()));
+		builder.append(", Subtotal: ");
+		builder.append(nf.format(getSubTotal()));
+		builder.append("\n");
+		return builder.toString();
 	}
 	
 	
