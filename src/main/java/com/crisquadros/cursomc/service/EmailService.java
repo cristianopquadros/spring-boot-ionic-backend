add3a70e785c4d1ec328d10aad2aa66c065ae384
cristianopquadros <cristianopquadros@gmail.com>
2020-10-13 17:26:07 -0300

    email html

--- src/main/java/com/crisquadros/cursomc/service/EmailService.java
+++ src/main/java/com/crisquadros/cursomc/service/EmailService.java
@@ -1,5 +1,7 @@
 package com.crisquadros.cursomc.service;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.crisquadros.cursomc.resources.domain.Pedido;
@@ -9,5 +11,10 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 	
 	void sendEmail(SimpleMailMessage msg);
+	
+    void sendHtmlEmail(MimeMessage msg);
+
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
 
 }
