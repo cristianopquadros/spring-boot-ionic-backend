7f160ddeb3f3b246d2679c023abb11b4116d4a1b
cristianopquadros <cristianopquadros@gmail.com>
2020-10-16 11:16:49 -0300

    Esqueci a senha

--- src/main/java/com/crisquadros/cursomc/service/AbstractEmailService.java
+++ src/main/java/com/crisquadros/cursomc/service/AbstractEmailService.java
@@ -13,6 +13,7 @@ import org.springframework.mail.javamail.MimeMessageHelper;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import com.crisquadros.cursomc.resources.domain.Cliente;
 import com.crisquadros.cursomc.resources.domain.Pedido;
 
 public abstract class AbstractEmailService implements EmailService{
@@ -68,6 +69,23 @@ public abstract class AbstractEmailService implements EmailService{
 		mmh.setSentDate(new Date(System.currentTimeMillis()));
 		mmh.setText(htmlFromTemplatePedido(obj), true);
 		return mimeMessage;
+	}
+	
+	@Override
+	public void sendNewPasswordEmail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = prepareNewPasswordEMail(cliente, newPass);
+		sendEmail(sm);	
+	}
+
+	protected SimpleMailMessage prepareNewPasswordEMail(Cliente cliente, String newPass) {
+		SimpleMailMessage sm = new SimpleMailMessage();
+		sm.setTo(cliente.getEmail());
+		sm.setFrom(sender);
+		sm.setSubject("Solicitacao de nova senha");
+		sm.setSentDate(new Date(System.currentTimeMillis()));
+		sm.setText("Nova Senha: "+newPass);
+		return sm;
+		
 	}
 
 }
--- src/main/java/com/crisquadros/cursomc/service/EmailService.java
+++ src/main/java/com/crisquadros/cursomc/service/EmailService.java
@@ -4,6 +4,7 @@ import javax.mail.internet.MimeMessage;
 
 import org.springframework.mail.SimpleMailMessage;
 
+import com.crisquadros.cursomc.resources.domain.Cliente;
 import com.crisquadros.cursomc.resources.domain.Pedido;
 
 public interface EmailService {
@@ -15,6 +16,8 @@ public interface EmailService {
     void sendHtmlEmail(MimeMessage msg);
 
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 	
 
 }
